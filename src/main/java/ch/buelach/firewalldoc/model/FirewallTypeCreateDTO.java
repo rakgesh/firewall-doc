e32d06e56501768904d95981d0c11f5e62f411e0
ganesrak <ganesrak@students.zhaw.ch>
2022-11-14 20:37:14 +0100

    Closes #47

---
+++ src/main/java/ch/buelach/firewalldoc/model/FirewallTypeCreateDTO.java
@@ -0,0 +1,13 @@
+package ch.buelach.firewalldoc.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class FirewallTypeCreateDTO {
+    
+    private String name;
+    private String description;
+
+}
