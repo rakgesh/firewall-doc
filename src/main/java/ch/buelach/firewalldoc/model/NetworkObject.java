5c31b5a395709b49daa0c34a18ec22fc404785c7
ganesrak <ganesrak@students.zhaw.ch>
2022-11-14 20:08:07 +0100

    Closes #6

---
+++ src/main/java/ch/buelach/firewalldoc/model/NetworkObject.java
@@ -0,0 +1,30 @@
+package ch.buelach.firewalldoc.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Document("network-object")
+public class NetworkObject {
+
+    @Id
+    private String id;
+    @NonNull
+    private String name;
+    @NonNull 
+    private String ip;
+    @NonNull
+    private String subnet;
+    @NonNull
+    private String description;
+    
+}
