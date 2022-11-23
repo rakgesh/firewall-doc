25adb8ac2e8c6c87a0e0fb2839f718ebfe7e07ae
ganesrak <ganesrak@students.zhaw.ch>
2022-11-23 15:01:52 +0100

    Frontend edited

---
+++ frontend/src/pages/Use-Cases.svelte
@@ -0,0 +1,164 @@
+<script>
+    import axios from "axios";
+  
+  
+    // TODO: Setze hier die URL zu deinem mit Postman erstellten Mock Server
+    const api_root = "http://localhost:8080";
+  
+    let useCases = [];
+    let useCase = {
+      name: null,
+      description: null,
+    };
+  
+    function getUseCases() {
+      var config = {
+        method: "get",
+        url: api_root + "/use-case",
+        headers: {},
+      };
+  
+      axios(config)
+        .then(function (response) {
+            useCases = response.data;
+        })
+        .catch(function (error) {
+          alert("Could not get Use Cases");
+          console.log(error);
+        });
+    }
+    getUseCases();
+  
+    function createUseCase() {
+      var config = {
+        method: "post",
+        url: api_root + "/use-case",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: useCase,
+      };
+  
+      axios(config)
+        .then(function (response) {
+          alert("Use Case created");
+          getUseCases();
+        })
+        .catch(function (error) {
+          alert("Could not create USe Case");
+          console.log(error);
+        });
+    }
+  
+  
+    
+    let sortBy = {col: "name", ascending: true};
+      
+      $: sort = (column) => {
+          
+          if (sortBy.col == column) {
+              sortBy.ascending = !sortBy.ascending
+          } else {
+              sortBy.col = column
+              sortBy.ascending = true
+          }
+          
+          // Modifier to sorting function for ascending or descending
+          let sortModifier = (sortBy.ascending) ? 1 : -1;
+          
+          let sort = (a, b) => 
+              (a[column] < b[column]) 
+              ? -1 * sortModifier 
+              : (a[column] > b[column]) 
+              ? 1 * sortModifier 
+              : 0;
+          
+        useCases = useCases.sort(sort);
+      }
+  
+  
+  
+  </script>
+
+<div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <h3 style="margin-top: 15px; font-weight: bold;">All Use Cases</h3>
+      </div>
+      <div class="col" />
+      <div class="col" style="text-align-last: right;">
+        <button
+          type="button"
+          class="btn"
+          data-toggle="modal" data-target="#crateUC"
+          style="margin-top: 9px; background-color: #c73834; color: #fff"
+          >Add Use Case</button
+        >
+      </div>
+    </div>
+  </div>
+  <table class="table table-striped table-hover" id="allUseCases">
+    <thead>
+      <tr>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <th scope="col">Name  <span on:click={sort("name")}> <i class="fa fa-fw fa-sort"></i></span></th>
+        <th scope="col" on:click={sort("description")}>Description  <i class="fa fa-fw fa-sort"></i></th>
+        <th scope="col" ></th>
+        <th scope="col"></th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each useCases as useCase}
+        <tr>
+          <td>{useCase.name}</td>
+          <td>{useCase.description}</td>
+          <td>edit</td>
+          <td>delete</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+  <p> Bearbeiten | Löschen </p>
+  
+  <div class="modal fade" id="crateUC" tabindex="-1" role="dialog" aria-labelledby="formCreateUseCase" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="crateUseCase">Add Use-Case</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form class="mb-5">
+            <div class="row mb-3">
+              <div class="col">
+                <label class="form-label" for="name">Name</label>
+                <input
+                  bind:value={useCase.name}
+                  class="form-control"
+                  id="name"
+                  type="text"
+                />
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col">
+                <label class="form-label" for="description">Description</label>
+                <input
+                  bind:value={useCase.description}
+                  class="form-control"
+                  id="description"
+                  type="text"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn" style="background-color: #008000; color: #fff" on:click={createUseCase}>Add</button>
+        </div>
+      </div>
+    </div>
+  </div>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,15 +1,28 @@
 
 import Home from "./pages/Home.svelte";
-import HostGroupObjects from "./pages/Host-Group-Objects.svelte";
-import HostObjects from "./pages/Host-Objects.svelte";
+import FirewallRules from "./pages/Firewall-Rules.svelte";
+import Contexts from "./pages/Contexts.svelte";
 import NetworkObjects from "./pages/Network-Objects.svelte";
 import NetworkGroupObjects from "./pages/Network-Group-Objects.svelte";
+import HostObjects from "./pages/Host-Objects.svelte";
+import HostGroupObjects from "./pages/Host-Group-Objects.svelte";
+import ServiceGroupObjects from "./pages/Service-Group-Objects.svelte";
+import UseCases from "./pages/Use-Cases.svelte";
+
+
+
 
 export default {
     '/': Home,
     '/home': Home,
-    '/host-Group-Objects': HostGroupObjects,
+    '/firewall-Rules': FirewallRules,
+    '/contexts': Contexts,
     '/network-Objects' : NetworkObjects,
-    '/host-Objects': HostObjects,
     '/network-Group-Objects': NetworkGroupObjects,
+    '/host-Objects': HostObjects,
+    '/host-Group-Objects': HostGroupObjects,
+    '/service-Group-Objects': ServiceGroupObjects,
+    '/use-cases': UseCases,
+    
+    
 }
