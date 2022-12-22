b31c232ea6fa8b225e063b42f058e9597605bcb6
rakgesh <ganesrak@students.zhaw.ch>
2022-12-22 11:42:00 +0100

    Closes #100

--- src/main/java/ch/buelach/firewalldoc/service/EmailServiceImpl.java
+++ src/main/java/ch/buelach/firewalldoc/service/EmailServiceImpl.java
@@ -5,8 +5,6 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
-
-
 @Component
 public class EmailServiceImpl {
 
@@ -15,43 +13,51 @@ public class EmailServiceImpl {
 
     public void sendMessageRequested(String to, String fwId) {
 
-        SimpleMailMessage message = new SimpleMailMessage(); 
+        SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("firewall.doc@gmail.com");
-        message.setTo(to); 
-        message.setSubject("New firewall rule requested"); 
-        message.setText("A new firewall rule has been registered in the documentation. To see the details and to accept or reject the rule, follow the provided link: http://localhost:8080/#/firewall-rule-details/" + fwId);
+        message.setTo(to);
+        message.setSubject("New firewall rule requested");
+        message.setText(
+                "A new firewall rule has been registered in the documentation. To see the details and to accept or reject the rule, follow the provided link: https://firewall-doc-1671475944450.azurewebsites.net/#/firewall-rule-details/"
+                        + fwId);
         emailSender.send(message);
     }
 
     public void sendMessageEdited(String to, String fwId) {
 
-        SimpleMailMessage message = new SimpleMailMessage(); 
+        SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("firewall.doc@gmail.com");
-        message.setTo(to); 
-        message.setSubject("A firewall rule has been edited"); 
-        message.setText("A firewall rule has been edited in the documentation. To see the details and to accept or reject the rule, follow the provided link: http://localhost:8080/#/firewall-rule-details/" + fwId);
+        message.setTo(to);
+        message.setSubject("A firewall rule has been edited");
+        message.setText(
+                "A firewall rule has been edited in the documentation. To see the details and to accept or reject the rule, follow the provided link: https://firewall-doc-1671475944450.azurewebsites.net/#/firewall-rule-details/"
+                        + fwId);
         emailSender.send(message);
     }
 
     public void sendMessageApproved(String to, String fwId) {
 
-        SimpleMailMessage message = new SimpleMailMessage(); 
+        SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("firewall.doc@gmail.com");
-        message.setTo(to); 
-        message.setSubject("Your firewall rule has been approved"); 
-        message.setText("Your firewall rule http://localhost:8080/#/firewall-rule-details/" + fwId + " has been approved by an admin. Please take the next steps and order the rule in the service portal of netcloud https://netcloud.service-now.com/csm. As soon as you have placed the order, set the state to ordered.");
+        message.setTo(to);
+        message.setSubject("Your firewall rule has been approved");
+        message.setText(
+                "Your firewall rule https://firewall-doc-1671475944450.azurewebsites.net/#/firewall-rule-details/"
+                        + fwId
+                        + " has been approved by an admin. Please take the next steps and order the rule in the service portal of netcloud https://netcloud.service-now.com/csm. As soon as you have placed the order, set the state to ordered.");
         emailSender.send(message);
     }
-    
+
     public void sendMessageRejected(String to, String admin, String fwId) {
 
-        SimpleMailMessage message = new SimpleMailMessage(); 
+        SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("firewall.doc@gmail.com");
-        message.setTo(to); 
-        message.setSubject("Your firewall rule has been rejected"); 
-        message.setText("Your firewall rule http://localhost:8080/#/firewall-rule-details/" + fwId + " has been rejected by " + admin);
+        message.setTo(to);
+        message.setSubject("Your firewall rule has been rejected");
+        message.setText(
+                "Your firewall rule https://firewall-doc-1671475944450.azurewebsites.net//#/firewall-rule-details/"
+                        + fwId + " has been rejected by " + admin);
         emailSender.send(message);
     }
-    
-    
+
 }
